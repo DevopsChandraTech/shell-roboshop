7374348cd52d74c49583351342cf3f47bbdbe82d
DevOpsChandraTech <devopschandra42@gmail.com>
2026-02-27 17:13:21 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -4,7 +4,7 @@ for instance in $@;
 do
     # Get instance Id from ec2 instance 
     # Give double quotes("") ResourceType
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0f5bdd34affdd5ed7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=test}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0f5bdd34affdd5ed7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress')
