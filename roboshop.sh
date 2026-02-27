06df1ec53cb35e00a02bce18fa084ebd8fe9c7d9
DevOpsChandraTech <devopschandra42@gmail.com>
2026-02-27 17:55:58 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+AMI_ID="ami-0220d79f3f480ecf5"
+SG_ID="sg-0f5bdd34affdd5ed7"
+HOSTZONE_ID=""Z0502082140Q7QA6WMLFW
+DOMAIN_NAME="devaws.shop"
+
 for instance in $@;
 do
     # Get instance Id from ec2 instance 
     # Give double quotes("") ResourceType
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0f5bdd34affdd5ed7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress')
@@ -13,6 +18,25 @@ do
     fi
 
     echo "$instance : $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $HOSTZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating Records to Route53"
+        ,"Changes": [{
+        "Action"              : "UPSERT" #if not create,if exist update
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$instance.$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'" $IP "'"
+            }]
+        }
+        }]
+    }
+    '
 done
 
 
