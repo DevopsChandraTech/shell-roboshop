2723faba95689f9e2b6811056b6ea1d1259f9aae
DevOpsChandraTech <devopschandra42@gmail.com>
2026-02-27 20:44:48 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -5,37 +5,37 @@ SG_ID="sg-0f5bdd34affdd5ed7"
 HOSTZONE_ID="Z0502082140Q7QA6WMLFW"
 DOMAIN_NAME="devaws.shop"
 
-for instance in $@;
+for instance in $@ # mongodb redis mysql
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
+    # Get Private IP
     if [ $instance != "frontend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress')
-        RECORD_NAME="$instance.$DOMAIN_NAME"
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws86s.fun
     else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress')
-        RECORD_NAME="$DOMAIN_NAME"
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME" # daws86s.fun
     fi
 
-    echo "$instance : $IP"
+    echo "$instance: $IP"
 
-aws route53 change-resource-record-sets \
+    aws route53 change-resource-record-sets \
     --hosted-zone-id $HOSTZONE_ID \
-    --change-batch '{
-        "Comment": "Updating record set",
-        "Changes": [{
-            "Action": "UPSERT",
-            "ResourceRecordSet": {
-                "Name": "'$RECORD_NAME'",
-                "Type": "A",
-                "TTL": 1,
-                "ResourceRecords": [{
-                    "Value": "'$IP'"
-                }]
-            }
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
         }]
-    }'
-
+    }
+    '
 done
-
-
