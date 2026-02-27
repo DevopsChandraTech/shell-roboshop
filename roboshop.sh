75263e4f22ab06137d9622a2ff11e01d0146ffb7
DevOpsChandraTech <devopschandra42@gmail.com>
2026-02-27 18:07:02 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -19,7 +19,9 @@ do
 
     echo "$instance : $IP"
 
-    aws route53 change-resource-record-sets --hosted-zone-id $HOSTZONE_ID --change-batch '
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $HOSTZONE_ID \
+    --change-batch '
     {
         "Comment": "Updating Records to Route53"
         ,"Changes": [{
