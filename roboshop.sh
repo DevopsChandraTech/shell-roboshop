d188d95225262d9773ab50862ce3d7c3b4ad21f8
DevOpsChandraTech <devopschandra42@gmail.com>
2026-02-27 17:11:51 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -7,13 +7,12 @@ do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0f5bdd34affdd5ed7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=test}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ $instance != "frontend" ]; then
-        PRIVATE_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress')
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress')
     else
-        PUBLICE_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress')
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress')
     fi
 
-    echo "$instance : $PUBLIC_IP"
-    echo "$instance : $PRIVATE_IP"
+    echo "$instance : $IP"
 done
 
 
