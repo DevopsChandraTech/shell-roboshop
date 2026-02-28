f25f9653791e55df11e8b59261dff68b76125fe6
DevOpsChandraTech <devopschandra42@gmail.com>
2026-02-28 21:51:56 +0530

    shell-roboshop

---
+++ catalogue.sh
@@ -0,0 +1,89 @@
+#/bin/bash
+#colours for script
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+#log folder creation 
+LOG_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+SCRIPT_DIR=$pwd
+HOST_IP="mongodb.devaws.shop"
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+mkdir -p /var/log/shell-roboshop
+
+# checks user with root priviliges or not
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]; then
+    echo "Error:: Run Command With Root User Privilizes."
+    exit 1
+fi
+
+# validation for script exist or not
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R Failure $N"
+        exit 1
+    else
+        echo -e "$2 ... $G Success $N"
+    fi
+}
+
+
+dnf module disable nodejs -y | tee -a $LOG_FILE
+VALIDATE $? "Disable Nodejs Module"
+
+dnf module enable nodejs:20 -y | tee -a $LOG_FILE
+VALIDATE $? "Enable Nodejs Module"
+
+dnf install nodejs -y | tee -a $LOG_FILE
+VALIDATE $? "Install Nodejs"
+
+#check here user exist or not
+id roboshop
+if [ id -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "Creating User"
+else 
+    echo -e "User already exist... $Y SKIPPING $N"
+fi
+
+mkdir -p /app | tee -a $LOG_FILE
+VALIDATE $? "Create Directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip | tee -a $LOG_FILE
+VALIDATE $? "Downloading Code"
+
+cd /app 
+VALIDATE $? "Change Directory"
+
+unzip /tmp/catalogue.zip
+VALIDATE $? "Unzip Code in tmp"
+
+cd /app 
+VALIDATE $? "Change Directory"
+
+npm install  | tee -a $LOG_FILE
+VALIDATE $? "Install Dependencies"
+
+systemctl daemon-reload
+systemctl enable catalogue | tee -a $LOG_FILE
+VALIDATE $? "Enable Catalogue"
+
+systemctl start catalogue | tee -a $LOG_FILE
+VALIDATE $? "Start Catalogue"
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copy Code"
+
+dnf install mongodb-mongosh -y | tee -a $LOG_FILE
+VALIDATE $? "Install mongodb Client"
+
+mongosh --host $HOST_IP </app/db/master-data.js
+VALIDATE $? "Load Products"
+
+systemctl restart catalogue | tee -a $LOG_FILE
+VALIDATE $? "Restart Service"
+
--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,5 @@
 #/bin/bash
 #colours for script
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -32,27 +31,27 @@ VALIDATE(){
 
 #copy mongodb repo
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? Adding Mongo Repo 
+VALIDATE $? "Adding Mongo Repo" 
 
 #install mongodb
 dnf install mongodb-org -y &>> $LOG_FILE
-VALIDATE $? Installing Mongo Repo
+VALIDATE $? "Installing Mongo Repo"
 
 # enable mongodb
 systemctl enable mongod | tee -a $LOG_FILE
-VALIDATE $? Enable Mongo Repo
+VALIDATE $? "Enable Mongo Repo"
 
 #start mongodb
 systemctl start mongod | tee -a $LOG_FILE
-VALIDATE $? Start Mongo Repo
+VALIDATE $? "Start Mongo Repo"
 
 # using sed update ip address for remote connection
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-VALIDATE $? Adding Remote Connection
+VALIDATE $? "Adding Remote Connection"
 
 #restart mongodb service
 systemctl restart mongod
-VALIDATE $? Restart Mongodb Service
+VALIDATE $? "Restart Mongodb Service"
 
 
 
