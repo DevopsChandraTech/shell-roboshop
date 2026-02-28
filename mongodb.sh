81af22a5ac8fb7ce647253ead4df70ec06884fb2
DevOpsChandraTech <devopschandra42@gmail.com>
2026-02-28 16:58:18 +0530

    shell-roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -57,7 +57,7 @@ N="\e[0m"
 LOG_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f2)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.sh"
-mkdir -p /var/log/shell-script
+mkdir -p /var/log/shell-roboshop
 
 
 VALIDATE(){
