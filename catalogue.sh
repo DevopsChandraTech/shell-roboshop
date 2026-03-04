6d386d48fa14579c461c9da162313fec719cdb5b
DevOpsChandraTech <devopschandra42@gmail.com>
2026-03-04 15:00:37 +0530

    shell-roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -9,7 +9,7 @@ N="\e[0m"
 LOG_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 SCRIPT_DIR=$PWD #this is special variable for current Directory
-MONGODB_HOST="mongodb.devaws.shop"
+MONGODB_HOST=mongodb.devaws.shop
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 mkdir -p /var/log/shell-roboshop
 
