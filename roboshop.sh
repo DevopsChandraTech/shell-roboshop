e3677de65ad47f0aa1fa7dbd8b6e1558b71ddec8
DevOpsChandraTech <devopschandra42@gmail.com>
2026-06-14 18:46:58 +0530

    shell-roboshop

---
+++ roboshop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+
+AMI_ID="ami-0220d79f3f480ecf5"
+SG_ID="sg-0f5bdd34affdd5ed7"
+
+#$@ -> taking all given arguments while running the script
+
+for instance in $@
+do
+   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0f5bdd34affdd5ed7 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' --query 'Instances[0].InstanceId' --output text)
+
+   if [ frontend -ne 0 ]; then
+    IP=$(aws ec2 describe-instances --instance-ids i-0bc3817e5c17034f1 --query "Reservations[0].Instances[0].NetworkInterfaces[0].PrivateIpAddress" --output text)
+   else
+    IP=$(aws ec2 describe-instances --instance-ids i-0bc3817e5c17034f1 --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+   fi
+   echo "$instance=$IP"
+
+
+done
