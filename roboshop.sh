10c0f2fdf9084230a53bb5c68388e50af3453d9c
DevOpsChandraTech <devopschandra42@gmail.com>
2026-06-14 21:21:01 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -5,10 +5,10 @@ SG_ID="sg-0f5bdd34affdd5ed7"
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids \ sg-0f5bdd34affdd5ed7 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=test}]' --query 'Instances[0].\ InstanceId' --output text)
+    INSTANCE_ID=123
 
     if [ $instance != "frontend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids i-0bc3817e5c17034f1 --query "Reservations[0].Instances[0].\ NetworkInterfaces[0].PrivateIpAddress" --output text)
+        IP=345
     else
         IP=$(aws ec2 describe-instances --instance-ids i-0bc3817e5c17034f1 --query 'Reservations[0].Instances[0]. \ PublicIpAddress' --output text)
     fi
