f30456fc6942db00ab2a1982fa6dbf16e5d2042f
DevOpsChandraTech <devopschandra42@gmail.com>
2026-06-14 20:45:37 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -3,17 +3,32 @@
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-0f5bdd34affdd5ed7"
 
-for instance in $@
+for instance in $@ # mongodb redis mysql
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID \ --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
+    # Get Private IP
     if [ $instance != "frontend" ]; then
-        # IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-        echo "frontend"
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        
     fi
 
     echo "$instance: $IP"
 
-done
+# for instance in $@
+# do
+#     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID \ --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+
+#     if [ $instance != "frontend" ]; then
+#         # IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+#         echo "frontend"
+#     else
+#         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+#     fi
+
+#     echo "$instance: $IP"
+
+# done
