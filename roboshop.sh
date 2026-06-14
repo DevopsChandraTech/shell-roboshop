33d3fc86580426c02e1a53a824edf5292953b839
DevOpsChandraTech <devopschandra42@gmail.com>
2026-06-14 21:58:17 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -5,7 +5,7 @@ SG_ID="sg-0f5bdd34affdd5ed7"
 
 for instance in $@
 do
-    INSTANCE_ID=INSTANCE_ID=$(aws ec2 run-instances \
+    INSTANCE_ID=$(aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type t3.micro \
     --security-group-ids $SG_ID \
