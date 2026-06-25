75553c0e326c676e8b985cc72591f159fcdc4e12
DevOpsChandraTech <devopschandra42@gmail.com>
2026-06-25 12:52:16 +0530

    shell-roboshop

--- redis.sh
+++ redis.sh
@@ -38,6 +38,7 @@ VALIDATE $? "Install redis"
 
 #sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "allowing remote connections"
 
 systemctl enable redis &>> $LOG_FILE
 VALIDATE $? "Enable redis"
