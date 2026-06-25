1715831ea1ae5847271a8bcbc2159575c1094083
DevOpsChandraTech <devopschandra42@gmail.com>
2026-06-25 20:34:52 +0530

    shell-roboshop

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -38,8 +38,13 @@ systemctl enable rabbitmq-server &>> $LOG_FILE
 VALIDATE $? "Enable Server"
 systemctl start rabbitmq-server &>> $LOG_FILE
 VALIDATE $? "Start the Server"
-rabbitmqctl add_user roboshop roboshop123 &>> $LOG_FILE
-VALIDATE $? "Added User"
+id roboshop
+if [ $? -ne 0 ]; then
+    rabbitmqctl add_user roboshop roboshop123 &>> $LOG_FILE
+    VALIDATE $? "Added User"
+else 
+    echo -e "roboshop user already added $Y Skipping..! $N"
+fi
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $LOG_FILE
 VALIDATE $? "Set Permissions to User"
 
